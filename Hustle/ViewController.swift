76492faf1305466fbc24081246473e9e1f19c444
amsaif52 <am52saif@gmail.com>
2017-07-23 20:02:57 -0400

    Initial Commit

---
+++ Hustle/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Hustle
+//
+//  Created by Ali Asgar Merchant on 7/23/17.
+//  Copyright © 2017 Ali Asgar Merchant. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
