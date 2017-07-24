c3317680fe7a5d9e783605183cb9970d7ba71b09
amsaif52 <am52saif@gmail.com>
2017-07-23 21:43:53 -0400

    Coded and Attached images to display the rocket animating after a button is pressed

--- Hustle/ViewController.swift
+++ Hustle/ViewController.swift
@@ -7,19 +7,48 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var powerBtn: UIButton!
+    @IBOutlet weak var darkBlueBG: UIImageView!
+    @IBOutlet weak var hustleLbl: UILabel!
+    @IBOutlet weak var cloudHolder: UIView!
+    @IBOutlet weak var rocket: UIImageView!
+    @IBOutlet weak var onLbl: UILabel!
+    
+    var player: AVAudioPlayer!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        let path = Bundle.main.path(forResource: "hustle-on", ofType: "wav")!
+        let url = URL(fileURLWithPath: path)
+        
+        do{
+            player = try AVAudioPlayer(contentsOf: url)
+            player.prepareToPlay()
+            
+        } catch let error as NSError{
+            print(error.description)
+        }
+        
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    @IBAction func powerBtnPressed(_ sender: Any) {
+        cloudHolder.isHidden = false
+        darkBlueBG.isHidden = true
+        powerBtn.isHidden = true
+        
+        player.play()
+        
+        UIView.animate(withDuration: 2.3, animations: {
+            self.rocket.frame = CGRect(x: 0, y: 130, width: 375, height: 402)
+        }) { (finished) in
+            self.onLbl.isHidden = false
+            self.hustleLbl.isHidden = false
+        }
     }
-
 
 }
 
